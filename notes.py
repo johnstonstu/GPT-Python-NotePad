ff338a0125976535109bad230e47aea468116ec5
johnstonstu <stu.w.johnston@gmail.com>
2023-04-17 00:09:19 -0700

    notepad

--- notes.py
+++ notes.py
@@ -7,6 +7,16 @@ def save_note():
         file.write(note + "\n")
     text_box.delete(1.0, END)
 
+# create a function to view the saved notes
+def view_notes():
+    with open("notes.txt", "r") as file:
+        notes = file.read()
+    notes_window = Toplevel(root)
+    notes_window.title("Saved Notes")
+    notes_text = Text(notes_window, height=10, width=50)
+    notes_text.pack()
+    notes_text.insert(END, notes)
+
 # create the GUI
 root = Tk()
 root.title("Note Taking App")
@@ -18,6 +28,10 @@ text_box.pack()
 # create a save button that saves the note to a file when clicked
 save_button = Button(root, text="Save", command=save_note)
 save_button.pack()
+
+# create a view button that opens a new window with the saved notes
+view_button = Button(root, text="View Notes", command=view_notes)
+view_button.pack()
 
 # start the main loop of the program
 root.mainloop()
